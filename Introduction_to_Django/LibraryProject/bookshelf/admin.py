3f8d69de899085b22d0742935ebb49e4faad526f
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-07 13:50:16 +0300

    More trials.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Book, Author
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author__name')
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-# Create your models here.
 
-class Book(models.Model):
-    title = models.CharField(max_length = 200)
-    author = models.CharField(max_length = 100)
-    publication_year = models.IntegerField()
-
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.name
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
