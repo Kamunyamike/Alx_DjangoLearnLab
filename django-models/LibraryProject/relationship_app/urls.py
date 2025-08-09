53970c6e0158034fba5129799492de1affc85ad4
Mikenickson Wanjohi <139453170+Kamunyamike@users.noreply.github.com>
2025-08-09 11:23:22 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .views import LibraryDetailView, book_list
+from .views import list_books
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('', views.book_list, name='home'),  # Redirect to book list on home page
+
 ]
