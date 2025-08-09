157f0b779c48a2240d3f2531f433e5f83950eebd
Mikenickson Wanjohi <139453170+Kamunyamike@users.noreply.github.com>
2025-08-09 11:17:26 +0300

    Some change in the relationship_app/views.py

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book, Library
+
+
+# Function-based view for listing books
+def book_list(request):
+    books = Book.objects.select_related('author').all()  # Optimized query
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# Class-based view for listing books
+class BookListView(ListView):
+    model = Book
+    template_name = 'relationship_app/book_list.html'
+    context_object_name = 'books'
+
+
+# Class-based view for displaying a library’s details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
