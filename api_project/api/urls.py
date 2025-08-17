a10ea5b1cfa0d0d4e3da24a11e05289324f6b667
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-17 14:23:34 +0300

    Update the Urls.py files.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from .views import BookListCreateAPIView
 from django.urls import include, path
 from .views import BookListAPIView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('books/', BookListAPIView.as_view(), name='book-list'),
-    path("api/", include("api.urls")),
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,8 +3,17 @@ from rest_framework import generics
 from .models import MyModel
 from .serializers import MyModelSerializer
 from .serializers import BookSerializer
+from rest_framework import viewsets
+from .models import MyModel
+from .serializers import MyModelSerializer
+from rest_framework import generics
+
 # Create your views here.
 
 class BookListAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
