6ff756055fb499b885d88572bd0c82695afd9a92
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-17 12:37:47 +0300

    Updated the urls.py.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,6 @@ from .views import BookListCreateAPIView
 from django.urls import include, path
 
 urlpatterns = [
-    path("api/books/", BookListCreateAPIView.as_view(), name="book_list_create"),
+    path('books/', BookListAPIView.as_view(), name='book-list'),
     path("api/", include("api.urls")),
 ]
