c7b12cd4d7334f01dc6e74925643860468dc9c09
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-17 12:51:42 +0300

    Updates on the urls.py.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import BookListCreateAPIView
 from django.urls import include, path
-
+from .views import BookListAPIView
 urlpatterns = [
     path('books/', BookListAPIView.as_view(), name='book-list'),
     path("api/", include("api.urls")),
