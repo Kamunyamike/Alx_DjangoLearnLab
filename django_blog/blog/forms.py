9ae8fac5a6327a10d402efd75e0dea6e406ad6c8
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-21 23:41:22 +0300

    Updated the forms.py file.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -6,6 +6,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        # Use the 'widgets' dictionary to apply a custom widget to a field.
+        widgets = {
+            'tags': forms.TextInput(attrs={'class': 'tag-input-field', 'placeholder': 'Tags separated by commas'}),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
