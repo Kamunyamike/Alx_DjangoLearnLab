1f6d1eefccf15038f40d4ef985d95bb404606cca
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-22 08:15:43 +0300

    Updated the blog/urls.py file.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,200 +1,34 @@
-# blog/views.py
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import (
-    ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView
+# blog/urls.py
+from django.urls import path
+from .views import (
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView,
+    register_view, login_view, profile_view, logout_view,
+    CommentCreateView, CommentUpdateView, CommentDeleteView,
+    search_view, posts_by_tag, PostByTagListView # Import the new class
 )
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib import messages
-from django.urls import reverse_lazy, reverse
-from datetime import datetime
-from django.db.models import Q
-from taggit.models import Tag
-from .models import Post, Comment
-from .forms import CommentForm, PostForm
 
-# ... (all your other view functions)
-
-# New Class-Based View for Tagged Posts
-class PostByTagListView(ListView):
-    model = Post
-    template_name = 'blog/posts_by_tag.html'
-    context_object_name = 'posts'
-    paginate_by = 5
-
-    def get_queryset(self):
-        # Get the tag from the URL and filter posts
-        tag_slug = self.kwargs.get('tag_slug')
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        return Post.objects.filter(tags__in=[tag]).order_by('-created_at')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        tag_slug = self.kwargs.get('tag_slug')
-        context['tag'] = get_object_or_404(Tag, slug=tag_slug)
-        return context
-
-# The rest of your views go here, including the original posts_by_tag function if you want to keep it.
-# You can remove the old posts_by_tag function if you want, but it's not strictly necessary.
-
-# User Authentication Views
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful. Welcome!')
-            return redirect('profile')
-    else:
-        form = UserCreationForm()
-    return render(request, 'blog/register.html', {'form': form})
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, f'Welcome back, {username}!')
-                return redirect('profile')
-            else:
-                messages.error(request, 'Invalid username or password.')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'blog/login.html', {'form': form})
-
-def logout_view(request):
-    logout(request)
-    messages.info(request, 'You have been logged out.')
-    return redirect('login')
-
-@login_required
-def profile_view(request):
-    return render(request, 'blog/profile.html')
-
-# Post Views
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-    ordering = ['-created_at']
-    paginate_by = 5
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
+urlpatterns = [
+    # Blog Post URLs (CRUD)
+    path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comment_form'] = CommentForm()
-        return context
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post_list')
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post_list')
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-# Comment Views
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
+    # User Authentication URLs
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile_view, name='profile'),
     
-    def form_valid(self, form):
-        pk = self.kwargs.get('pk')
-        post = get_object_or_404(Post, pk=pk)
-        
-        form.instance.post = post
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    # Comment URLs
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 
-    def get_success_url(self):
-        return reverse('post_detail', kwargs={'pk': self.object.post.pk})
-
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/comment_form.html'
-
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-    def get_success_url(self):
-        return reverse('post_detail', kwargs={'pk': self.object.post.pk})
-
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    template_name = 'blog/comment_confirm_delete.html'
-    
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-    def get_success_url(self):
-        return reverse('post_detail', kwargs={'pk': self.object.post.pk})
-
-# New Views for Search and Tagging
-def search_view(request):
-    query = request.GET.get('q')
-    posts = Post.objects.none()
-    
-    if query:
-        posts = Post.objects.filter(
-            Q(title__icontains=query) |
-            Q(content__icontains=query) |
-            Q(tags__name__icontains=query)
-        ).distinct().order_by('-created_at')
-
-    context = {
-        'query': query,
-        'posts': posts
-    }
-    return render(request, 'blog/search_results.html', context)
-
-def posts_by_tag(request, tag_slug):
-    tag = get_object_or_404(Tag, slug=tag_slug)
-    posts = Post.objects.filter(tags__in=[tag]).order_by('-created_at')
-    
-    context = {
-        'tag': tag,
-        'posts': posts,
-    }
-    return render(request, 'blog/posts_by_tag.html', context)
+    # New Tagging and Search URLs
+    path('search/', search_view, name='search'),
+    # Update this line to use the new class-based view
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
+]
