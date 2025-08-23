e7d511d10b1f233e153d45571af6f2de9fa09e1c
Kamunyamike <kamunyawanjohi@gmail.com>
2025-08-23 14:37:16 +0300

    Updated the urls.py file in the accounts app to include the user_id.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow'),
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
 ]
